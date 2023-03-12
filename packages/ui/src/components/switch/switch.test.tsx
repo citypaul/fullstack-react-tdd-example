5a3da3a85284c392932e4ee7ac45f01610f86d2c
Paul Hammond <paul.hammond@gmail.com>
2023-03-12 16:03:27 +0000

    Initial setup for switch component

---
+++ packages/ui/src/components/switch/switch.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { Switch } from "./switch";
+
+describe("Switch", () => {
+  it("should...", () => {
+    render(<Switch />);
+  });
+});
