bdfc74a6c341498402fb8f839d50169192e2fcb3
Paul Hammond <paul.hammond@gmail.com>
2023-03-12 16:03:27 +0000

    Initial setup for switch component

---
+++ packages/ui/src/components/switch/switch.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+import { Switch } from ".";
+
+export default {
+  title: "Shared Library/Switch",
+  args: {
+    on: false,
+  },
+  component: Switch,
+} as ComponentMeta<typeof Switch>;
+
+const Template: ComponentStory<typeof Switch> = (args) => <Switch {...args} />;
+
+export const Primary = Template.bind({});
