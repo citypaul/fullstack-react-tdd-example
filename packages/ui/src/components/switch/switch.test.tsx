5cb2859e563affc47738fc5730c784d673281972
Paul Hammond <paul.hammond@gmail.com>
2023-03-12 17:01:28 +0000

    switch component passes all tests

--- packages/ui/src/components/switch/switch.test.tsx
+++ packages/ui/src/components/switch/switch.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import { Switch } from "./switch";
 
@@ -21,6 +22,23 @@ describe("Switch", () => {
     expect(switchElement).toBeChecked();
 
     rerender(<Switch on={false} />);
+
+    expect(switchElement).not.toBeChecked();
+  });
+
+  it("should be possible to turn on and off using user events", async () => {
+    render(<Switch />);
+
+    const switchElement = screen.getByRole("switch");
+
+    expect(switchElement).toBeInTheDocument();
+    expect(switchElement).not.toBeChecked();
+
+    await userEvent.click(switchElement);
+
+    expect(switchElement).toBeChecked();
+
+    await userEvent.click(switchElement);
 
     expect(switchElement).not.toBeChecked();
   });
