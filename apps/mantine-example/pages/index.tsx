87324f1893be67ef613d3f8a19702a4c3babd92d
Paul Hammond <paul.hammond@gmail.com>
2023-03-25 15:12:41 +0000

    modal component complete

--- apps/mantine-example/pages/index.tsx
+++ apps/mantine-example/pages/index.tsx
@@ -1,14 +1,26 @@
-import { Button, Text, Timeline } from '@mantine/core';
+import { Anchor, Button, Flex, Group, Text, Timeline } from '@mantine/core';
+import {
+  AccessibleModalExample,
+  ModalExample,
+} from '../components/AccessibleModalExample/AccessibleModalExample';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { InaccessibleModalExample } from '../components/InaccessibleModalExample/InaccessibleModalExample';
-import { ModalExample } from '../components/ModalExample/ModalExample';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
-      <InaccessibleModalExample />
+      <Flex justify={'center'} align="center" direction={'column'} gap="md">
+        <Anchor href="https://mantine.dev/core/dialog/" target={'_blank'}>
+          Mantine docs (Dialog component)
+        </Anchor>
+        <InaccessibleModalExample />
+        <Anchor href="https://mantine.dev/core/modal/" target={'_blank'}>
+          Mantine docs (Modal component)
+        </Anchor>
+        <AccessibleModalExample />
+      </Flex>
     </>
   );
 }
