8cd2f614870a81f59d2c200344b93d721730fdf3
Paul Hammond <paul.hammond@gmail.com>
2023-03-26 15:19:07 +0100

    wip

--- apps/frontend/src/components/FormExample.test.tsx
+++ apps/frontend/src/components/FormExample.test.tsx
@@ -1,1 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { FormExample } from "./FormExample";
+
+describe("FormExample", () => {
+  it("can submit the form", () => {
+    const firstName = "John";
+    const email = "john@test.com";
+    const password = "password";
+
+    render(<FormExample />);
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const emailInput = screen.getByLabelText(/email address/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    userEvent.type(firstNameInput, firstName);
+    userEvent.type(emailInput, email);
+    userEvent.type(passwordInput, password);
+
+    const submitButton = screen.getByRole("button", { name: /submit/i });
+    userEvent.click(submitButton);
+
+    expect(screen.getByText(/form submitted/i)).toBeInTheDocument();
+  });
+});
--- apps/frontend/src/components/FormExample.tsx
+++ apps/frontend/src/components/FormExample.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const FormExample = () => {
   return (
     <form className="max-w-xs flex flex-col gap-4">
-      <Input label="First name" />
+      <Input label="First name" className="text-red-500" />
       <Input label="Email Address" type={"email"} />
       <Input label="Password" type={"password"} />
       <Button>Submit</Button>
