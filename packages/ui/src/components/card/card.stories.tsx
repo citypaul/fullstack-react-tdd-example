d5aade5f4302c99308731e79fed7b5dd966abd0a
Paul Hammond <paul.hammond@gmail.com>
2023-03-27 20:53:04 +0100

    created markup and css for card component (#6)

--- packages/ui/src/components/card/card.stories.tsx
+++ packages/ui/src/components/card/card.stories.tsx
@@ -2,13 +2,19 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
 
 import { Card } from ".";
+// ts-ignore is needed because the image is not a module
+// @ts-ignore
+import image from "./story-images/shoe.jpg";
 
 export default {
   title: "Shared Library/Card",
-  args: {},
+  args: {
+    img: image,
+    imgAlt: "Shoe",
+  },
   component: Card,
 } as ComponentMeta<typeof Card>;
 
-const Template: ComponentStory<typeof Card> = (args) => <Card />;
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
 
 export const Primary = Template.bind({});
--- packages/ui/src/components/card/card.tsx
+++ packages/ui/src/components/card/card.tsx
@@ -1,5 +1,28 @@
 import React from "react";
 
-export const Card = () => {
-  return <div>Card</div>;
+export type CardProps = {
+  img: string;
+  imgAlt: string;
+};
+
+export const Card = (props: CardProps) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={props.img} alt={props.imgAlt} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-between items-end">
+          <button className="btn btn-primary">Buy Now</button>
+
+          <div>
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
