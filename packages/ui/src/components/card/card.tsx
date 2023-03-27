75056cfb716ffdde85e1f0ed5f6c9713c3ffd573
Paul Hammond <paul.hammond@gmail.com>
2023-03-27 20:39:26 +0100

    fix ts issue

--- packages/ui/src/components/card/card.tsx
+++ packages/ui/src/components/card/card.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import image from "./story-images/shoe.jpg";
 
-export const Card = () => {
+export type CardProps = {
+  img: string;
+  imgAlt: string;
+};
+
+export const Card = (props: CardProps) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src={image} alt="Shoes" />
+        <img src={props.img} alt={props.imgAlt} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">Shoes!</h2>
