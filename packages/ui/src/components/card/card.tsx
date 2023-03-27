3089fb0a8284af8818dcf5cd10aea7ad0e50a709
Paul Hammond <paul.hammond@gmail.com>
2023-03-27 20:21:50 +0100

    created markup and css for card component

--- packages/ui/src/components/card/card.tsx
+++ packages/ui/src/components/card/card.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import image from "./story-images/shoe.jpg";
 
 export const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-between items-end">
+          <button className="btn btn-primary">Buy Now</button>
+
+          <div>
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
