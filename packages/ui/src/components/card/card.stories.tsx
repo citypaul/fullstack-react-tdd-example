1a96ef9207e1072215bb3e6462273c575251a08a
Paul Hammond <paul.hammond@gmail.com>
2023-03-27 22:56:33 +0100

    Created card react component

--- packages/ui/src/components/card/card.stories.tsx
+++ packages/ui/src/components/card/card.stories.tsx
@@ -8,13 +8,30 @@ import image from "./story-images/shoe.jpg";
 
 export default {
   title: "Shared Library/Card",
-  args: {
-    img: image,
-    imgAlt: "Shoe",
-  },
   component: Card,
 } as ComponentMeta<typeof Card>;
 
-const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+const Template: ComponentStory<typeof Card> = () => (
+  <Card>
+    <Card.Image src={image} alt="shoe" />
+
+    <Card.Body>
+      <Card.Header>
+        <Card.Title>Card Title</Card.Title>
+      </Card.Header>
+
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+
+      <Card.Actions>
+        <button className="btn btn-primary">Buy Now</button>
+
+        <div>
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </Card.Actions>
+    </Card.Body>
+  </Card>
+);
 
 export const Primary = Template.bind({});
