ad2c4daeeffbe33748cb7a4843abe01bca661f9a
Paul Hammond <paul.hammond@gmail.com>
2023-03-28 22:34:50 +0100

    first product card test passing (#11)

--- apps/frontend/src/components/product-card/ProductCard.tsx
+++ apps/frontend/src/components/product-card/ProductCard.tsx
@@ -1,7 +1,6 @@
-import { Card } from "fullstack-react-tdd-example-ui";
+import { Badge, Card } from "fullstack-react-tdd-example-ui";
 import React from "react";
 
-// export price and product type to types package
 type Price = {
   currency: "£" | "$" | "€";
   value: number;
@@ -13,6 +12,7 @@ type Image = {
 };
 
 export type Product = {
+  id: string;
   title: string;
   description: string;
   image: Image;
@@ -20,8 +20,31 @@ export type Product = {
   tags: string[];
 };
 
-export type ProductCardProps = { product: Product };
+export type ProductCardProps = {
+  onClick: (productId: Product["id"]) => void;
+  product: Product;
+};
 
-export const ProductCard = (props: ProductCardProps) => {
-  return <></>;
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <Card>
+      <Card.Image src={product.image.src} alt={product.image.alt} />
+      <Card.Title>{product.title}</Card.Title>
+      <Card.Body>
+        <p>{product.price.currency + product.price.value}</p>
+        <p>{product.description}</p>
+      </Card.Body>
+
+      <Card.Actions>
+        <button className="btn btn-primary">Buy Now</button>
+        <div>
+          {product.tags.map((tag) => (
+            <Badge outline key={tag}>
+              {tag}
+            </Badge>
+          ))}
+        </div>
+      </Card.Actions>
+    </Card>
+  );
 };
