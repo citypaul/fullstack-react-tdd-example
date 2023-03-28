f2330a6981ab4832086cb4552b7394e6183e7b6a
Paul Hammond <paul.hammond@gmail.com>
2023-03-28 21:10:49 +0100

    Initial setup for product card

---
+++ apps/frontend/src/components/product-card/ProductCard.test.tsx
@@ -0,0 +1,1 @@
+import { ProductCard } from "./";
---
+++ apps/frontend/src/components/product-card/ProductCard.tsx
@@ -0,0 +1,13 @@
+import { Card } from "fullstack-react-tdd-example-ui";
+import React from "react";
+
+export type ProductCardProps = {
+  title: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+  price: number;
+  tags: string[];
+};
+
+export const ProductCard = () => {};
