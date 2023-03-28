9ae7b791fad1a41df96946a3687533d237c489fe
Paul Hammond <paul.hammond@gmail.com>
2023-03-28 18:28:45 +0100

    Created card react component (#8)

--- packages/ui/src/components/card/card.stories.tsx
+++ packages/ui/src/components/card/card.stories.tsx
@@ -8,13 +8,61 @@ import image from "./story-images/shoe.jpg";
 
 export default {
   title: "Shared Library/Card",
-  args: {
-    img: image,
-    imgAlt: "Shoe",
-  },
   component: Card,
 } as ComponentMeta<typeof Card>;
 
-const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+export const CardWithImage: ComponentStory<typeof Card> = () => (
+  <Card>
+    <Card.Image src={image} alt="shoe" />
 
-export const Primary = Template.bind({});
+    <Card.Body>
+      <Card.Title>
+        Card Title <span className="badge badge-secondary">New!</span>
+      </Card.Title>
+
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+
+      <Card.Actions>
+        <button className="btn btn-primary">Buy Now</button>
+
+        <div>
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </Card.Actions>
+    </Card.Body>
+  </Card>
+);
+
+export const WithBottomImage: ComponentStory<typeof Card> = () => (
+  <Card>
+    <Card.Body>
+      <Card.Title>
+        Card Title <span className="badge badge-secondary">New!</span>
+      </Card.Title>
+
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+
+      <Card.Actions>
+        <button className="btn btn-primary">Buy Now</button>
+      </Card.Actions>
+    </Card.Body>
+    <Card.Image src={image} alt="shoe" />
+  </Card>
+);
+
+export const CardWithNoImage: ComponentStory<typeof Card> = () => (
+  <Card>
+    <Card.Body>
+      <Card.Title>
+        Card Title <span className="badge badge-secondary">New!</span>
+      </Card.Title>
+
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+
+      <Card.Actions>
+        <button className="btn btn-primary">Buy Now</button>
+      </Card.Actions>
+    </Card.Body>
+  </Card>
+);
--- packages/ui/src/components/card/card.tsx
+++ packages/ui/src/components/card/card.tsx
@@ -1,28 +1,52 @@
 import React from "react";
 
-export type CardProps = {
-  img: string;
-  imgAlt: string;
+export type CardImageProps = {
+  src: string;
+  alt: string;
 };
 
-export const Card = (props: CardProps) => {
-  return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={props.img} alt={props.imgAlt} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-between items-end">
-          <button className="btn btn-primary">Buy Now</button>
+const CardComponent = ({ children }: { children: React.ReactNode }) => (
+  <div className="card w-96 bg-base-100 shadow-xl">{children}</div>
+);
 
-          <div>
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+CardComponent.Title = ({ children }: { children: React.ReactNode }) => (
+  <h2 className="card-title">{children}</h2>
+);
+CardComponent.Image = ({ src, alt }: CardImageProps) => (
+  <figure>
+    <img className="card-image" src={src} alt={alt} />
+  </figure>
+);
+CardComponent.Body = ({ children }: { children: React.ReactNode }) => (
+  <div className="card-body">{children}</div>
+);
+CardComponent.Footer = ({ children }: { children: React.ReactNode }) => (
+  <div className="card-footer">{children}</div>
+);
+CardComponent.Actions = ({ children }: { children: React.ReactNode }) => (
+  <div className="card-actions justify-between items-end">{children}</div>
+);
+
+export const Card = CardComponent;
+
+// export const Card = (props: CardProps) => {
+//   return (
+//     <div className="card w-96 bg-base-100 shadow-xl">
+//       <figure>
+//         <img src={props.img} alt={props.imgAlt} />
+//       </figure>
+//       <div className="card-body">
+//         <h2 className="card-title">Shoes!</h2>
+//         <p>If a dog chews shoes whose shoes does he choose?</p>
+//         <div className="card-actions justify-between items-end">
+//           <button className="btn btn-primary">Buy Now</button>
+
+//           <div>
+//             <div className="badge badge-outline">Fashion</div>
+//             <div className="badge badge-outline">Products</div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
