5ad9f91ef0475a8affe084841f6a2c361531b74d
Paul Hammond <paul.hammond@gmail.com>
2023-03-28 23:36:23 +0100

    product card callback test passing and product card storybook story (#12)

--- apps/frontend/src/components/product-card/ProductCard.test.tsx
+++ apps/frontend/src/components/product-card/ProductCard.test.tsx
@@ -36,4 +36,37 @@ describe("ProductCard", () => {
     expect(screen.getByText(tags[1])).toBeInTheDocument();
     expect(screen.getByText("£10")).toBeInTheDocument();
   });
+
+  it('should trigger a callback with the product id when the "Buy Now" button is clicked', () => {
+    const title = "Product Title";
+    const description = "Product Description";
+    const imageSrc = "https://example.com/image.png";
+    const imageAlt = "Product Image";
+    const tags = ["tag1", "tag2"];
+    const onClick = jest.fn();
+
+    render(
+      <ProductCard
+        product={{
+          id: "1",
+          title,
+          description,
+          image: {
+            src: imageSrc,
+            alt: imageAlt,
+          },
+          price: {
+            currency: "£",
+            value: 10,
+          },
+          tags,
+        }}
+        onClick={onClick}
+      />
+    );
+
+    screen.getByText("Buy Now").click();
+
+    expect(onClick).toHaveBeenCalledWith("1");
+  });
 });
