1099092631beb4a6a154da819c8a8d6892091e0f
Paul Hammond <paul.hammond@gmail.com>
2023-03-28 23:36:23 +0100

    product card callback test passing and product card storybook story (#12)

--- apps/frontend/src/components/product-card/ProductCard.tsx
+++ apps/frontend/src/components/product-card/ProductCard.tsx
@@ -25,26 +25,37 @@ export type ProductCardProps = {
   product: Product;
 };
 
-export const ProductCard = ({ product }: ProductCardProps) => {
+export const ProductCard = ({ onClick, product }: ProductCardProps) => {
   return (
     <Card>
       <Card.Image src={product.image.src} alt={product.image.alt} />
-      <Card.Title>{product.title}</Card.Title>
-      <Card.Body>
-        <p>{product.price.currency + product.price.value}</p>
-        <p>{product.description}</p>
-      </Card.Body>
 
-      <Card.Actions>
-        <button className="btn btn-primary">Buy Now</button>
-        <div>
-          {product.tags.map((tag) => (
-            <Badge outline key={tag}>
-              {tag}
-            </Badge>
-          ))}
-        </div>
-      </Card.Actions>
+      <Card.Body>
+        <Card.Title>
+          {product.title}{" "}
+          <Badge type="secondary" outline>
+            {`${product.price.currency}${product.price.value}`}
+          </Badge>
+        </Card.Title>
+
+        <p>{product.description}</p>
+
+        <Card.Actions>
+          <button
+            className="btn btn-primary"
+            onClick={() => onClick(product.id)}
+          >
+            Buy Now
+          </button>
+          <div>
+            {product.tags.map((tag) => (
+              <Badge outline key={tag}>
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        </Card.Actions>
+      </Card.Body>
     </Card>
   );
 };
