7a66ad81832831096566104eb2dd6328e46a7acf
Paul Hammond <paul.hammond@gmail.com>
2023-03-29 18:39:40 +0100

    created initial test utils

---
+++ apps/frontend/src/test-utils/test-utils.tsx
@@ -0,0 +1,14 @@
+import { render, RenderOptions } from "@testing-library/react";
+import React, { ReactElement } from "react";
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
