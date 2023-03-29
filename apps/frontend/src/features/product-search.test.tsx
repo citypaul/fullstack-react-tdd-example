494ca0e9db5573cfaf3ccb8ac42d92cd1f186339
Paul Hammond <paul.hammond@gmail.com>
2023-03-29 20:32:01 +0100

    stub tests for product search, test utils & React Query setup (#17)

---
+++ apps/frontend/src/features/product-search.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "../test-utils";
+
+// For our purposes, we are going to assume if data is returned
+// from the API, it will always be valid and complete
+
+// Proper error handling and handling of invalid data should be tested
+// at the BFF level.
+
+describe("ProductSearch", () => {
+  it("if products are returned, they should be displayed as a list of product cards", () => {});
+
+  it.todo("if no products are returned, a message should be displayed");
+  it.todo("if an error occurs, a message should be displayed");
+});
