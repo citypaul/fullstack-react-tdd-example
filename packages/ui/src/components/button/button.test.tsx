69f8642dfa239af1c8442d22402c2c40537c574d
Paul Hammond <paul.hammond@gmail.com>
2023-03-29 22:33:32 +0100

    buttons accepting onclick handler (#19)

--- packages/ui/src/components/button/button.test.tsx
+++ packages/ui/src/components/button/button.test.tsx
@@ -3,6 +3,19 @@ import React from "react";
 import { Button } from "./button";
 
 describe("Button", () => {
+  it('should call the "onClick" handler when clicked', () => {
+    const expectedButtonText = "Click me!";
+    const onClick = jest.fn();
+
+    render(<Button onClick={onClick}>{expectedButtonText}</Button>);
+
+    expect(onClick).not.toHaveBeenCalled();
+
+    screen.getByRole("button", { name: expectedButtonText }).click();
+
+    expect(onClick).toHaveBeenCalled();
+  });
+
   it("should render with the correct class name for primary button", () => {
     const expectedButtonText = "Click me!";
 
