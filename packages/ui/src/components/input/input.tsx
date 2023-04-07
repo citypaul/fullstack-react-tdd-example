3a6f5b1f40a7102dc079e1f4e912c01e1a4864f7
Paul Hammond <paul.hammond@gmail.com>
2023-04-07 16:37:01 +0100

    label: first test passing

--- packages/ui/src/components/input/input.tsx
+++ packages/ui/src/components/input/input.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useId } from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
 }
 
-export const Input = (props: InputProps) => {
+export const Input = ({ label, ...rest }: InputProps) => {
+  const id = useId();
+
   return (
     <div className="form-control w-full max-w-xs">
-      <label className="label">
-        <span className="label-text">This is a label</span>
+      <label className="label" htmlFor={id}>
+        <span className="label-text">{label}</span>
       </label>
       <input
-        type="text"
+        {...rest}
+        id={id}
         placeholder="Type here"
         className="input input-bordered w-full max-w-xs"
       />
