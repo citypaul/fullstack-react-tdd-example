8c38264b02db2dfc4b9d084a5b9a777f7430f38f
Paul Hammond <paul.hammond@gmail.com>
2023-04-16 14:11:43 +0100

    added react router and site menu

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
+import {
+  NavLink,
+  Outlet,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Counter } from "./components";
 import { ProductSearch } from "./features";
 import { store } from "./store";
@@ -9,13 +17,81 @@ if (process.env.NODE_ENV === "development") {
   worker.start();
 }
 
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={
+        <>
+          <nav className="navbar bg-indigo-100 mb-4 rounded-md">
+            <ol className="menu menu-horizontal px-1">
+              <li>
+                <a
+                  href="http://localhost:3001"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Modal
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="http://localhost:6006"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Storybook
+                </a>
+              </li>
+
+              <li>
+                <NavLink
+                  to="counter-use-state"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                  Counter (use state)
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="counter-use-reducer"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                  Counter (use reducer)
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="product-search"
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                  Product Search
+                </NavLink>
+              </li>
+            </ol>
+          </nav>
+          <Outlet />
+        </>
+      }
+    >
+      <Route path="/">
+        <Route path="counter-use-state" element={<Counter />} />
+        <Route path="counter-use-reducer" element={<Counter />} />
+        <Route path="product-search" element={<ProductSearch />} />
+      </Route>
+    </Route>
+  )
+);
+
 export const App = () => {
   return (
     <Provider store={store}>
       <div className="container mx-auto p-4">
         <main>
-          <Counter />
-          <ProductSearch />
+          <RouterProvider router={router} />
         </main>
       </div>
     </Provider>
