3ae58ab0c73fad964d5444db463840aa19993d5f
Paul Hammond <paul.hammond@gmail.com>
2023-05-21 00:01:01 +0100

    remove font and unused import

--- apps/london-js/pages/index.tsx
+++ apps/london-js/pages/index.tsx
@@ -1,13 +1,9 @@
 import { Counter } from "@/components/counter";
-import { Button } from "fullstack-react-tdd-example-ui";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <div className="relative flex place-items-center">
         <Counter />
