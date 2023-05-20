bd1b4ec30fa8673b436ad00123ee7395b656c450
Paul Hammond <paul.hammond@gmail.com>
2023-05-20 14:28:17 +0100

    remove counter as a starting point for talk

--- apps/london-js/components/counter.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Counter } from "./counter";
-
-describe("counter", () => {
-  it("should render", () => {
-    render(<Counter />);
-    expect(screen.getByText("Count: 0")).toBeInTheDocument();
-  });
-});
