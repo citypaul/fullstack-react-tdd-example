d6e4f9857b623c0743ef1c139cf1ee68b28fa5f0
Paul Hammond <paul.hammond@gmail.com>
2023-05-20 11:07:07 +0100

    revert update due to storybook issues

--- packages/ui/src/components/input/input.test.tsx
+++ packages/ui/src/components/input/input.test.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Input } from "./input";
 
 describe("Input", () => {
-  it("renders and handles input", async () => {
+  it("renders and handles input", () => {
     const expectedLabel = "Name";
 
     const handleChange = jest.fn();
@@ -17,7 +17,7 @@ describe("Input", () => {
     );
 
     const input = screen.getByLabelText(expectedLabel);
-    await userEvent.type(input, "John Doe");
+    userEvent.type(input, "John Doe");
 
     expect((input as HTMLInputElement).value).toBe("John Doe");
     expect(handleChange).toHaveBeenCalledWith("John Doe");
