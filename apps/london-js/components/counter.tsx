4348a9c4ad398fd6cda22a52d7524c7bbe26ad51
Paul Hammond <paul.hammond@gmail.com>
2023-05-21 01:17:54 +0100

    try that

--- apps/london-js/components/counter.tsx
+++ apps/london-js/components/counter.tsx
@@ -1,1 +1,15 @@
-export const Counter = () => <h1>Hello World!</h1>;
+import { Button } from "fullstack-react-tdd-example-ui";
+
+export const Counter = () => (
+  <div className="stat">
+    <h2 className="stat-title">Count</h2>
+    <p className="stat-value">0</p>
+    <div className="stat-actions">
+      <div className="btn-group">
+        <Button>Increment</Button>
+        <Button>Decrement</Button>
+        <Button>Reset</Button>
+      </div>
+    </div>
+  </div>
+);
