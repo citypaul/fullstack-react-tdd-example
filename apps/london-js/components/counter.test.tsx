7ccfe1cd72ff8e1e5900f5791b87482c3b98caf3
Paul Hammond <paul.hammond@gmail.com>
2023-05-21 16:13:12 +0100

    use state implementation

--- apps/london-js/components/counter.test.tsx
+++ apps/london-js/components/counter.test.tsx
@@ -1,9 +1,96 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Counter } from "./counter";
 
-describe("hello-world", () => {
-  it("should render", () => {
+describe("counter component", () => {
+  it("Upon the Counter component's initial render, the counter value should be zero", () => {
     render(<Counter />);
-    expect(screen.getByText("Hello World!")).toBeInTheDocument();
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "0"
+    );
+  });
+
+  it('When user clicks the "Increment" button, the counter value should increase by one (1).', async () => {
+    render(<Counter />);
+
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+
+    await userEvent.click(incrementButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "1"
+    );
+
+    await userEvent.click(incrementButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "2"
+    );
+  });
+
+  it('When a user interacts with the "Decrement" button, the counter value should decrease by one (1) provided the current counter value is above zero (0).', async () => {
+    render(<Counter />);
+
+    const decrementButton = screen.getByRole("button", { name: "Decrement" });
+
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+
+    await userEvent.click(incrementButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "1"
+    );
+
+    await userEvent.click(decrementButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "0"
+    );
+  });
+
+  it("If a user attempts to decrement the counter value when it's already at zero (0), the counter value should remain at zero (0).", async () => {
+    render(<Counter />);
+
+    const decrementButton = screen.getByRole("button", {
+      name: "Decrement",
+    });
+
+    await userEvent.click(decrementButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "0"
+    );
+  });
+
+  it('When a user interacts with the "Reset" button, the counter value should reset to zero (0), regardless of its current value.', async () => {
+    render(<Counter />);
+
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+
+    const resetButton = screen.getByRole("button", { name: "Reset" });
+
+    await userEvent.click(incrementButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "1"
+    );
+
+    await userEvent.click(incrementButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "2"
+    );
+
+    await userEvent.click(resetButton);
+
+    expect(screen.getByRole("status", { name: "counter" })).toHaveTextContent(
+      "0"
+    );
   });
 });
+
+//
+//
+//
+// When a user interacts with the "Reset" button, the counter value should reset to zero (0), regardless of its current value.
--- apps/london-js/components/counter.tsx
+++ apps/london-js/components/counter.tsx
@@ -1,1 +1,52 @@
-export const Counter = () => <h1>Hello World!</h1>;
+import { Button } from "fullstack-react-tdd-example-ui";
+import { useReducer, useState } from "react";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => {
+      if (prevCount > 0) {
+        return prevCount - 1;
+      }
+
+      return 0;
+    });
+  };
+
+  const reset = () => setCount(0);
+
+  return (
+    <div className="stat">
+      <h2 className="stat-title">Count</h2>
+      <p className="stat-value" role="status" aria-label="counter">
+        {count}
+      </p>
+      <div className="stat-actions">
+        <div className="btn-group">
+          <Button onClick={increment}>Increment</Button>
+          <Button onClick={decrement}>Decrement</Button>
+          <Button onClick={reset}>Reset</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+{
+  /* <div className="stat">
+<h2 className="stat-title">Count</h2>
+<p className="stat-value">0</p>
+<div className="stat-actions">
+  <div className="btn-group">
+    <Button>Increment</Button>
+    <Button>Decrement</Button>
+    <Button>Reset</Button>
+  </div>
+</div>
+</div> */
+}
