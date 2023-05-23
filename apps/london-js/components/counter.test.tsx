9c0c4dbf12b40f9f797f5c0483f735f483c2b435
Paul Hammond <paul.hammond@gmail.com>
2023-05-24 00:07:33 +0100

    name describe block in counter test (#69)

--- apps/london-js/components/counter.test.tsx
+++ apps/london-js/components/counter.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Counter } from "./counter";
 
-describe("hello-world", () => {
+describe("Counter", () => {
   it("should render", () => {
     render(<Counter />);
     expect(screen.getByText("Hello World!")).toBeInTheDocument();
